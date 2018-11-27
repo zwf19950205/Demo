7c22b9b4be5762ea6dbe2e4e69879f77871a804e
zhuwenfeng <zhuwenfeng@zhuwenfengdeMacBook-Air.local>
2018-11-27 14:43:19 +0800

    添加一个注释

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+// 同步测试
 router.get('/', function(req, res, next) {
   res.send('respond with a resourceaaaaaaaaaaa');
 });
