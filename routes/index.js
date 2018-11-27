38a360fe1917f6a331e64e829b678e239b47578b
zhuwenfeng <zhuwenfeng@zhuwenfengdeMacBook-Air.local>
2018-11-27 15:16:45 +0800

    asdsadssd

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 // 我是新增的注释 2018年11月27日14:53:30
-// 我是新增的第二条注释 2018年11月27日15:15:04
+// 我是新增的第二条注释 2018年11月27日15:15:04dsadaassa
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Expressaaaaaaa' });
 });
