59106d2f59becc7b3ea60ec5b507d873391c2cf1
zhuwenfeng <zhuwenfeng@zhuwenfengdeMacBook-Air.local>
2018-11-27 14:53:46 +0800

    新增一条注释

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// 我是新增的注释 2018年11月27日14:53:30
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Expressaaaaaaa' });
 });
